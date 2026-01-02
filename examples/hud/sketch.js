d51b2a1d4fecb5836d6dd90a0abc701dcd2bdff2
nakednous <nakednous@gmail.com>
2026-01-01 19:44:21 -0500

    examples update

--- examples/hud/sketch.js
+++ examples/hud/sketch.js
@@ -7,34 +7,25 @@ async function setup() {
   createCanvas(600, 400, WEBGL);
   textFont(font);
   textSize(14);
+  console.log(p5.Tree.VERSION);
 }
 
 function draw() {
   background(20);
-
   orbitControl();
-
   ambientLight(120);
   directionalLight(255, 255, 255, 0.25, 0.3, -1);
-
   normalMaterial();
   box(140);
-
-  // --- BROKEN HUD (direct text in WEBGL) ---
+  // HUD working here
   beginHUD();
-
   push();
-  //resetMatrix(); // works even commented!
   translate(-width / 2, -height / 2);
-
   noStroke();
   fill(0, 160);
   rect(10, 10, 220, 56, 6);
-
   fill(255);
-  text('HUD OK\norbitControl OK', 20, 30); // <- text often invisible
-
+  text('HUD OK\norbitControl OK', 20, 30);
   pop();
-
   endHUD();
 }
